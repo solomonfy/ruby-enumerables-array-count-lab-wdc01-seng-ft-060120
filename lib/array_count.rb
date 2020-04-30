064279a458d34eda0cd8871823b53af904cb0b9f
SOLOMON YISMAW <fantahunsolomon@gmail.com>
2020-04-30 15:36:25 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -9,7 +9,7 @@ end
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 
-  #index = 0 
+  index = 0 
   array.count do |array|
     array[index] == ""
   end
